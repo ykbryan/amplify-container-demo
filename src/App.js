5d0251e78edb86fecc5bc910b29cce074da212fb
Bryan Chua <me@bryanchua.com>
2021-04-23 17:48:57 +0800

    add aws amplify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import API from '@aws-amplify/api';
+import Amplify from "@aws-amplify/core";
+
+import awsConfig from "./aws-exports";
+Amplify.configure(awsConfig);
+
 
 function App() {
+  useEffect(() => {
+    getMessage();
+  }, [])
+  
+  async function getMessage() {
+    const apiName = awsConfig.aws_cloud_logic_custom[0].name;
+    await API.get(apiName, '/', {})
+      .then(data => {
+        console.log(data)
+      }).catch(error => {
+        console.log(error.response);
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
